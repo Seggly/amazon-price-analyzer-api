280151836dff9aaee69ece14803f8607b51f90fc
Seggly <benoitd.r@hotmail.com>
2025-01-14 14:52:52 -0500

    g

--- content.js
+++ content.js
@@ -25,7 +25,7 @@ if (fabImg) {
 
     <div class="initial-view">
       <div class="mascot">
-        <img src="${chrome.runtime.getURL('icons/mascot.svg')}" alt="Mascot" />
+        <img src="${chrome.runtime.getURL('icons/icon128.png')}" alt="Mascot" />
       </div>
       <h2>Don't Buy Until Our AI Check The Price First!</h2>
       <button class="analyze-button">Analyze The Price</button>
